af982d249b51bca390cbded27b9d860deae948ba
hubbeli <n3peve00@students.oamk.fi>
2024-09-12 10:17:05 +0300

    Projektin juurikansio ja sen alikansiot css-, img-, ja js- luotu. Alikansioihin luotu .css ja .js tiedostot ja viety 1-6.png-tiedostot. Luotu index.hmtl-tiedosto.

---
+++ js/functions.js
@@ -0,0 +1,10 @@
+
+const dice = document.getElementById('dice');
+const diceImage = document.getElementById('image');
+
+function roll() {
+    const randomNumber = Math.floor(Math.random() * 6) + 1;
+    image.src = `./img/${randomNumber}.png`;
+}
+
+dice.addEventListener('click', roll);
